e28599e5d955b1eced67010c71b6236d6c480844
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-10-06 06:00:01 +0800

    added type explicit type for nameOrCode

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,45 @@
 export = multibase;
 
 declare namespace multibase {
+  export type name =
+    'base1'
+    | 'base2'
+    | 'base8'
+    | 'base10'
+    | 'base16'
+    | 'base32'
+    | 'base32pad'
+    | 'base32hex'
+    | 'base32hexpad'
+    | 'base32z'
+    | 'base58flickr'
+    | 'base58btc'
+    | 'base64'
+    | 'base64pad'
+    | 'base64url'
+    | 'base64urlpad'
+    ;
+  export type code =
+    '1'
+    | '0'
+    | '7'
+    | '9'
+    | 'f'
+    | 'b'
+    | 'c'
+    | 'v'
+    | 't'
+    | 'h'
+    | 'Z'
+    | 'z'
+    | 'm'
+    | 'M'
+    | 'u'
+    | 'U'
+    ;
+
   export function encode(
-    nameOrCode: string | number,
+    nameOrCode: name | code,
     buf: Buffer | string
   ): Buffer;
 
